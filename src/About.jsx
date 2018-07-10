660b4aa037628c6a8eeb3dbe081b46e58fac72a2
KennyCruzer <kennycruz@Kenneths-MacBook-Pro.local>
2018-07-10 14:46:38 -0400

    css about page

--- src/About.jsx
+++ src/About.jsx
@@ -12,46 +12,47 @@ export default function About(){
 
             <div className="photo"></div>
 
+            <div className="section">
+              <span>who?</span><br />
+              I'm Kenneth Cruz,
+              but everyone except my mom calls me Kenny <br />
+              You'll be able to find me on social media as Kenny Cruzer <br/>
+              (where the name come from isn't as interesting as the things I do)
+              I'm a graphic designer and full stack developer <br/>
+              born, raised and working in the greater area of NYC.
+              <br />
+              I specialize in branding, design for social media, packaging, <br />
+              visual communication for brands,
+              and full stack applications for clients.
+            </div>
 
-            <span>who?</span><br /><br />
-            I'm Kenneth Cruz,
-            but everyone except my mom calls me Kenny <br />
-            You'll be able to find me on social media as Kenny Cruzer <br/>
-            (where the name come from isn't as interesting as the things I do)
-            I'm a graphic designer and full stack developer <br/>
-            born, raised and working in the greater area of NYC.
-            <br />
-            I specialize in branding, design for social media, packaging, <br />
-            visual communication for brands,
-            and full stack applications for clients.
-            <br />
-            <br />
-            <span>Languages/Technologies/Frameworks</span> <br /><br />
-            English<br />
-            Spanish<br />
-            Italian<br /><br />
-            Abobe Photoshop CC <br />
-            Abobe Illustrator CC <br />
-            Abobe InDesign CC <br />
-            Abobe Dreamweaver CC <br />
-            Abobe Muse CC <br />
-            <br />
-            HTML5 <br />
-            CSS3 <br />
-            SCSS <br />
-            JavaScript ES6<br />
-            Ruby <br />
-            <br />
-            jQuery <br />
-            Express.js <br />
-            Node.js <br />
-            React.js <br />
-            Rails <br />
-            Bulma <br />
-            Bootstrap <br />
-            <br />
-
-            <a href="mailto:kenny@knnyczr.com"> kenny@knnyczr.com </a>
+            <div className="section">
+                <span>Languages/Technologies/Frameworks</span> <br />
+                English<br />
+                Spanish<br />
+                Italian<br /><br />
+                Abobe Photoshop CC <br />
+                Abobe Illustrator CC <br />
+                Abobe InDesign CC <br />
+                Abobe Dreamweaver CC <br />
+                Abobe Muse CC <br />
+                <br />
+                HTML5 <br />
+                CSS3 <br />
+                SCSS <br />
+                JavaScript ES6<br />
+                Ruby <br />
+                <br />
+                jQuery <br />
+                Express.js <br />
+                Node.js <br />
+                React.js <br />
+                Rails <br />
+                Bulma <br />
+                Bootstrap <br />
+                <br />
+                <a href="mailto:kenny@knnyczr.com"> kenny@knnyczr.com </a>
+            </div>
           </div>
 
         </div>
