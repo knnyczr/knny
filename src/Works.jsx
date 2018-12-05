3357dcc64a6d044d8fa4c205a8464a87862e6119
KennyCruzer <kennycruz@Kenneths-MacBook-Pro.local>
2018-12-05 13:41:58 -0500

    new macbook

--- src/Works.jsx
+++ src/Works.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './css/Works.css';
-import posters from './images/01232017_hero.jpg';
-import likwid from './images/LIKWID.png';
-import thesis from './images/thesis.jpg';
-import amys from './images/amys.png';
-import spectrum from './images/spectrum.png';
-import ulog from './images/ulog.png';
-import hue from './images/ilovehue.png';
-import palet from './images/pal_et.png';
-import tictactoe from './images/tictactoe.png';
+// import posters from './images/01232017_hero.jpg';
+// import likwid from './images/LIKWID.png';
+// import thesis from './images/thesis.jpg';
+// import amys from './images/amys.png';
+// import spectrum from './images/spectrum.png';
+// import ulog from './images/ulog.png';
+// import hue from './images/ilovehue.png';
+// import palet from './images/pal_et.png';
+// import tictactoe from './images/tictactoe.png';
 import 'font-awesome/css/font-awesome.min.css';
 import FontAwesome from 'react-fontawesome';
 import localdatabase from './db.js'; 
@@ -45,47 +45,46 @@ export default class Works extends Component{
     }, 5000)
   }
   makingCards(){
-    console.log(localdatabase)
-    let inner = document.getElementsByClassName("inner")[0]
-    localdatabase.forEach((d, i)=>{
+    // console.log(localdatabase)
+    let inner = document.getElementsByClassName("inner")[0];
+    localdatabase.forEach((d, i) => {
       console.log(d, i)
-      inner.append(
-        // console.log(d[i].github)
-        <p>${d.title}</p>
-      //   <div className="card">
-      //   <div className="card-image">
-      //     <figure className="image is-5by3">
-      //       <a href={d.github}><img style={{ backgroundImage: `url(${d.image})` }} /></a>
-      //     </figure>
-      //   </div>
+      return (
+      // console.log(d[i].github)
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-5by3">
+            <a href={d.github}><img style={{ backgroundImage: `url(${d.image})` }} /></a>
+          </figure>
+        </div>
 
-      //   <div className="card-content">
-      //     <div className="media">
+        <div className="card-content">
+          <div className="media">
 
-      //       <div className="media-content">
-      //         <p className="title is-4">Pal Et</p>
-      //       </div>
-      //     </div>
+            <div className="media-content">
+              <p className="title is-4">Pal Et</p>
+            </div>
+          </div>
 
-      //     <div className="content">
-      //       This is an educational app for developers and designers to learn about colors, and color relationships through a dynamic color picker.
-      //       <br />
-      //       <br />
+          <div className="content">
+            This is an educational app for developers and designers to learn about colors, and color relationships through a dynamic color picker.
+            <br />
+            <br />
 
-      //           <span><FontAwesome className="react" name='circle' /> React</span>
-      //           <span><FontAwesome className="express" name='circle' /> Express</span>
-      //           <span><FontAwesome className="bulma" name='circle' /> Bulma</span>
-      //           <span><FontAwesome className="api" name='circle' /> API</span>
+                <span><FontAwesome className="react" name='circle' /> React</span>
+                <span><FontAwesome className="express" name='circle' /> Express</span>
+                <span><FontAwesome className="bulma" name='circle' /> Bulma</span>
+                <span><FontAwesome className="api" name='circle' /> API</span>
 
-      //       <br />
-      //       <p><Link to='/works/thesis'></Link><br />
-      //       <a href={d.github}>view GitHub</a>
-      //       </p>
-      //       <br />
-      //     </div>
+            <br />
+            <p><Link to='/works/thesis'></Link><br />
+            <a href={d.github}>view GitHub</a>
+            </p>
+            <br />
+          </div>
 
-      //   </div>
-      // </div>
+        </div>
+      </div>
       )
     })
   }
@@ -110,7 +109,11 @@ export default class Works extends Component{
   render(){
     return(
       <div className="wrapper" onLoad={() => this.componentDidMount()}>
-        <div className="inner"></div>
+        <div className="inner">
+        {
+          console.log(localdatabase)
+        }
+        </div>
         <span id="loader">0%</span>
       </div>
     )
